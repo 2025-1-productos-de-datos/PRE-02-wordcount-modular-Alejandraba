229d0ebd28edbfdb32b1f32178ec995bd36f772a
Alejandraba <mbastidaspo@unal.edu.co>
2025-05-03 11:00:33 -0500

    separar funciones

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,25 +1,17 @@
 # obtain a list of files in the input directory
 
-from homework.src._internals.count_words import count_words
-from homework.src._internals.preprocess_lines import preprocess_lines
-from homework.src._internals.read_all_lines import read_all_lines
-from homework.src._internals.split_in_words import split_in_words
-
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_in_words import split_in_words
 from ._internals.write_count_words import write_count_words
 
 
 def main():
 
-    ## mover a la funcion "read_all_lines"
     all_lines = read_all_lines()
-
-    ## mover a "preprocess_lines"
     all_lines = preprocess_lines(all_lines)
-
-    ## mover "split_in_words"
     words = split_in_words(all_lines)
-
-    ## mover a "count_words"
     counter = count_words(words)
 
     # count the frequency of the words in the files in the input directory
